0b6cb76c35473cde66b61d63bb6e5582bb4e6458
himchan94 <71649055+healthcarefrontend@users.noreply.github.com>
2021-04-22 17:49:37 +0900

    bob is moving

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-// Fizz Buzz
-// 3으로 나눠지는 숫자는 Fizz
-// 5로 나눠지는 숫자는 Buzz
+const bob = document.querySelector(".face")
 
-// 15로 나눠지면 Fizz Buzz
+let count = 0
 
-const a = 10
-
-if (a % 15 === 0) {
-  console.log("Fizz Buzz")
-} else {
-  if (a % 3 === 0) {
-    console.log("Fizz")
-  } else if (a % 5 === 0) {
-    console.log("Buzz")
-  }
+function moveBob() {
+  count += 50
+  bob.style.left = count + "px"
 }
+
+bob.addEventListener("click", moveBob)
