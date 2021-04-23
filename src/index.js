067977372305f6de7b32286a6abc5835b80b7295
himchan94 <71649055+healthcarefrontend@users.noreply.github.com>
2021-04-23 14:33:52 +0900

    Array Wordk

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-//.slice()
-// it does not mutate arrays
-//it produce new ones
+// const celcius = [23, 140, 212, 41];
+// // -34 * 1.8
+// function getCelcius(far) {
+//   const cel = far.map((f) => (f - 32) / 1.8);
+//   console.log(cel);
+// }
 
-const display = document.querySelector(".display")
+// getCelcius(celcius);
+
+// const testList = [11, NaN, [], "Angels"];
+
+// function checkFalsey(arr) {
+//   return arr.some((element) => !element);  // NaN is falsey
+// }
+
+// console.log(checkFalsey(testList));
+
+const testList = ["Rabbit", "Football", "Cracking"];
+function getTotal() {
+  return testList.reduce((acc, current) => acc + current).length;
+}
+
+console.log(getTotal(testList));
